ccf80dedcea1e1fbf377267022658ec277f814e7
Alejandro Raya <alejandroraya09@hotmail.com>
2024-08-02 01:07:42 -0600

    Map

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState([ 'One PunchMan', 'Dragon Ball']) 
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+
+
+      <ol>
+        { categories.map(category => {
+          return <li key={category}> { category } </li>
+        }) }
+      </ol>
+
+      
+    </>
+  );
+
+
+};
